bf84aab2fb9b97dff4ec4c445d71a9a4d2aeca28
Maxim Seshuk <maxim.seshuk@gmail.com>
2021-10-05 01:57:22 +0000

    Update AwsS3Config

--- api/src/main/kotlin/com/briolink/companyservice/api/config/AwsS3Config.kt
+++ api/src/main/kotlin/com/briolink/companyservice/api/config/AwsS3Config.kt
@@ -1,5 +1,6 @@
 package com.briolink.companyservice.api.config
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.context.annotation.Bean
