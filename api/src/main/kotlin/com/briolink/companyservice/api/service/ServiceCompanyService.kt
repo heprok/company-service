9aa7c76f8417f030b0f68485e472dddaa6f2bfe4
Oleg Kravchuck <oleg@briolink.com>
2021-10-07 02:02:55 +0300

    fixed deploy

--- api/src/main/kotlin/com/briolink/companyservice/api/service/ServiceCompanyService.kt
+++ api/src/main/kotlin/com/briolink/companyservice/api/service/ServiceCompanyService.kt
@@ -2,22 +2,15 @@ package com.briolink.companyservice.api.service
 
 import com.briolink.companyservice.common.jpa.read.entity.ServiceReadEntity
 import com.briolink.companyservice.common.jpa.read.repository.ServiceReadRepository
-import com.briolink.companyservice.common.jpa.write.entity.UserWriteEntity
-import com.briolink.companyservice.common.jpa.write.repository.UserWriteRepository
 import com.briolink.companyservice.common.util.PageRequest
 import org.springframework.data.domain.Page
-import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
 class ServiceCompanyService(
     private val serviceReadRepository: ServiceReadRepository,
-    private val userRepository: UserWriteRepository
 ) {
-    fun createUser(user: UserWriteEntity): UserWriteEntity = userRepository.save(user)
-    fun updateUser(user: UserWriteEntity) = userRepository.save(user)
-    fun deleteUser(id: UUID) = userRepository.findById(id)
     fun getByCompanyId(id: UUID, limit: Int, offset: Int): Page<ServiceReadEntity> =
             serviceReadRepository.findByCompanyIdIs(id, PageRequest(offset, limit))
 }
