71fc81d7bbd731314c2c0a8383edefdef2ee1437
Oleg Kravchuck <oleg@briolink.com>
2021-11-02 16:44:36 +0300

    delete db from connection

--- updater/src/main/kotlin/com/briolink/companyservice/updater/dataloader/ConnectionDataLoader.kt
+++ updater/src/main/kotlin/com/briolink/companyservice/updater/dataloader/ConnectionDataLoader.kt
@@ -32,6 +32,7 @@ class ConnectionDataLoader(
     private var connectionServiceHandler: ConnectionHandlerService,
 ) : DataLoader() {
     override fun loadData() {
+        connectionReadRepository.deleteAll()
         if (
             connectionReadRepository.count().toInt() == 0 &&
             userReadRepository.count().toInt() != 0 &&
