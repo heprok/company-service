02b6e155f079c6ae85a11a2e140901949af016c2
Oleg Kravchuk <oleg@briolink.com>
2021-12-07 04:05:31 +0200

    Added event domain for companyservice sync connection

--- api/src/main/kotlin/com/briolink/companyservice/api/rest/ApiRest.kt
+++ api/src/main/kotlin/com/briolink/companyservice/api/rest/ApiRest.kt
@@ -4,8 +4,10 @@ import com.briolink.companyservice.api.dataloader.CompanyDataLoader
 import com.briolink.companyservice.api.dataloader.IndustryDataLoader
 import com.briolink.companyservice.api.dataloader.KeywordDataLoader
 import com.briolink.companyservice.api.dataloader.OccupationDataLoader
-import com.briolink.companyservice.common.domain.v1_0.Statistic
+import com.briolink.companyservice.common.domain.v1_0.*
+import com.briolink.companyservice.common.event.v1_0.CompanyConnectionEvent
 import com.briolink.companyservice.common.event.v1_0.StatisticRefreshEvent
+import com.briolink.companyservice.common.jpa.read.repository.ConnectionReadRepository
 import com.briolink.event.publisher.EventPublisher
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -20,9 +22,11 @@ class ApiRest(
     private val industryDataLoader: IndustryDataLoader,
     private val occupationDataLoader: OccupationDataLoader,
     private val keywordDataLoader: KeywordDataLoader,
+    private val connectionReadRepository: ConnectionReadRepository,
 ) {
     @GetMapping("/statistic/refresh")
     fun refreshStatistic(): ResponseEntity<Int> {
+
         eventPublisher.publishAsync(
             StatisticRefreshEvent(Statistic(null)),
         )
@@ -35,6 +39,51 @@ class ApiRest(
         occupationDataLoader.loadData()
         keywordDataLoader.loadData()
         companyDataLoader.loadData()
+        return ResponseEntity.ok(1)
+    }
+
+    @GetMapping("/connection")
+    fun loadDataConnection(): ResponseEntity<Int> {
+        connectionReadRepository.findAll().forEach {
+            eventPublisher.publish(
+                CompanyConnectionEvent(
+                    Connection(
+                        id = it.id,
+                        participantFrom = ConnectionParticipant(
+                            userId = it.participantFromUserId,
+                            userJobPositionTitle = it.data.participantFrom.userJobPositionTitle ?: "",
+                            companyId = it.participantFromCompanyId,
+                            companyRole = ConnectionCompanyRole(
+                                id = it.data.participantFrom.companyRole.id,
+                                name = it.data.participantFrom.companyRole.name,
+                                type = it.data.participantFrom.companyRole.type.let { if (it.value == 0) ConnectionCompanyRoleType.Buyer else ConnectionCompanyRoleType.Seller }
+                            )
+                        ),
+                        participantTo = ConnectionParticipant(
+                            userId = it.participantToUserId,
+                            userJobPositionTitle = it.data.participantTo.userJobPositionTitle ?: "",
+                            companyId = it.participantToCompanyId,
+                            companyRole = ConnectionCompanyRole(
+                                id = it.data.participantTo.companyRole.id,
+                                name = it.data.participantTo.companyRole.name,
+                                type = it.data.participantTo.companyRole.type.let { if (it.value == 0) ConnectionCompanyRoleType.Buyer else ConnectionCompanyRoleType.Seller }
+                            )
+                        ),
+                        services = it.data.services.map {
+                            ConnectionService(
+                                id = it.id,
+                                serviceId = it.serviceId,
+                                serviceName = it.serviceName,
+                                startDate = it.startDate,
+                                endDate = it.endDate
+                            )
+                        } as ArrayList<ConnectionService>,
+                        status = ConnectionStatus.valueOf(it.status.name),
+                        created = it.created
+                    )
+                )
+            )
+        }
         return ResponseEntity.ok(1)
     }
 }
--- common/src/main/kotlin/com/briolink/companyservice/common/event/v1_0/CompanyEvent.kt
+++ common/src/main/kotlin/com/briolink/companyservice/common/event/v1_0/CompanyEvent.kt
@@ -1,9 +1,11 @@
 package com.briolink.companyservice.common.event.v1_0
 
 import com.briolink.companyservice.common.domain.v1_0.Company
+import com.briolink.companyservice.common.domain.v1_0.Connection
 import com.briolink.companyservice.common.domain.v1_0.Domain
 import com.briolink.event.Event
 
 data class CompanyCreatedEvent(override val data: Company) : Event<Domain>("1.0")
 data class CompanyUpdatedEvent(override val data: Company) : Event<Domain>("1.0")
+data class CompanyConnectionEvent(override val data: Connection) : Event<Domain>("1.0")
 // data class CompanyDeletedEvent(override val data: Company) : Event<Domain>("1.0")
